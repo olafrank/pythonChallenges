05f4f78d6c79a3ab1df24152cdf1f250ea96e1bf
olafrank <olafrnk@gmail.com>
2023-10-06 17:20:14 +0100

    fizzbuss updated

--- fizzbuz.py
+++ fizzbuz.py
@@ -2,7 +2,7 @@ total = 0
 
 for number in range(1, 101):
     if number % 3 == 0 and number % 5 == 0:
-        print('fizzbuss')
+        print('fizz-buss')
     elif number % 5 == 0:
         print('fizz')
     elif number % 3 == 0:
